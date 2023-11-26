826d23802bcd2e3fa803f04204bf8f0f18480eef
Daniel Kofler <fox27374@gmail.com>
2023-11-26 11:07:01 +0100

    fixes sql module

--- radius.sql
+++ radius.sql
@@ -1,1 +1,4 @@
-SELECT * FROM radcheck;
+insert into nas (nasname, shortname, secret) values ('10.140.60.250', 'WLC', 'yLfeCF258ZRjntagVN9JDP');
+insert into nas (nasname, shortname, secret) values ('10.140.60.249', 'Testclient', 'yLfeCF258ZRjntagVN9JDP');
+insert into radcheck (username, attribute, op, value) values ('test', 'Cleartext-Password', ':=', 'test');
+insert into radcheck (username, attribute, op, value) values ('32d594f9a39c', 'Cleartext-Password', ':=', '32d594f9a39c');
