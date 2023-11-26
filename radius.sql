23bc0d1a15acae9782a583ad08c480c01af5dbe6
Daniel Kofler <fox27374@gmail.com>
2023-11-26 13:31:16 +0100

    moved variables to env file

--- radius.sql
+++ radius.sql
@@ -1,4 +1,25 @@
+# Clients
 insert into nas (nasname, shortname, secret) values ('10.140.60.250', 'WLC', 'yLfeCF258ZRjntagVN9JDP');
 insert into nas (nasname, shortname, secret) values ('10.140.60.249', 'Testclient', 'yLfeCF258ZRjntagVN9JDP');
+
+# Users
 insert into radcheck (username, attribute, op, value) values ('test', 'Cleartext-Password', ':=', 'test');
-insert into radcheck (username, attribute, op, value) values ('32d594f9a39c', 'Cleartext-Password', ':=', '32d594f9a39c');
+insert into radcheck (username, attribute, op, value) values ('f2270f1e2ea4', 'Cleartext-Password', ':=', 'f2270f1e2ea4');
+
+# User Group Mapping
+insert into radusergroup (username, groupname, priority) values ('test', 'TESTGROUP', '0');
+insert into radusergroup (username, groupname, priority) values ('f2270f1e2ea4', 'mobile', '0');
+
+# Radius reply by group
+# TESTGROUP
+insert into radgroupreply (groupname, attribute, op, value) values ('TESTGROUP', 'Tunnel-Type', '=', 'VLAN');
+insert into radgroupreply (groupname, attribute, op, value) values ('TESTGROUP', 'Tunnel-Medium-Type', '=', '802');
+insert into radgroupreply (groupname, attribute, op, value) values ('TESTGROUP', 'Tunnel-Private-Group-ID', '=', '63');
+
+# mobile
+insert into radgroupreply (groupname, attribute, op, value) values ('mobile', 'Tunnel-Type', '=', 'VLAN');
+insert into radgroupreply (groupname, attribute, op, value) values ('mobile', 'Tunnel-Medium-Type', '=', '802');
+insert into radgroupreply (groupname, attribute, op, value) values ('mobile', 'Tunnel-Private-Group-ID', '=', '60');
+
+update radcheck set value = 'yLfeCF258ZRjntagVN9JDP' where username = 'f2270f1e2ea4';
+update radgroupreply set value = '60' where attribute = 'Tunnel-Private-Group-ID';
